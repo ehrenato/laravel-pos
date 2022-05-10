2566e19473258a919005a22b83e715ccda9d58b8
ehrenato <ehrenato@gmail.com>
2022-05-10 10:03:38 -0300

    Adicionando Assets e front end

--- app/Http/Controllers/Site/CategoryController.php
+++ app/Http/Controllers/Site/CategoryController.php
@@ -2,21 +2,26 @@
 
 namespace App\Http\Controllers\Site;
 
-
-use Illuminate\Routing\Controller;
-use App\Category;
-
+use App\Models\Category;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        return view('site.category.index', ['categories' => Category::all(),]);
+        return view('site.category.index', [
+            'categories' => Category::all(),
+        ]);
     }
 
     public function show(Category $category)
     {
         return view('site.category.show', ['category' => $category->load('products')]);
     }
-
 }
--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,12 @@ Route::namespace('site')->group(function (){
 
     Route::get('/', [HomeController::class, 'index'])->name('site.home');
 
-    Route::get('produtos', [CategoryController::class, 'index'])->name('site.produtos');
+    Route::get('produtos', [CategoryController::class, 'index'])->name('site.products');
     Route::get('produtos/{slug}', [CategoryController::class, 'show'])->name('site.produtos.slug');
 
     Route::get('blog', [BlogController::class, 'index'])->name('site.blog');
 
-    Route::view('sobre', 'site.about');
+    Route::view('sobre', 'site.about.index')->name('site.about');
 
     Route::get('contato', [ContactController::class, 'index'])->name('site.contact');
     Route::post('contato', [ContactController::class, 'form'])->name('site.contact.form');
